be5d5e3dab11acfb7aec0f8efaef580783bf3c0c
freezge <freezge@gmail.com>
2023-04-20 03:21:25 +0600

    some comments for linked list

--- untitled/src/MyLinkedList.java
+++ untitled/src/MyLinkedList.java
@@ -1,28 +1,42 @@
 public class MyLinkedList<E> implements MyList<E>{
     private class Node <E> {
-        private E value;
-        private Node<E> next, prev;
-        public Node(E value){
+        private E value; //for value
+        private Node<E> next, prev;//next connected element and previous connected element
+        public Node(E value){//constructor for new values
             this.value = value;
         }
     }
-    private Node <E> head, tail;
-    private int size;
-    MyLinkedList(){ //constructor
+    private Node <E> head, tail;//head - first element, tail - last element
+    private int size;//size for size
+    MyLinkedList(){ //constructor for new list
         size = 0;
         head = null;
         tail = null;
     }
+    /*
+    * @size() returns param size
+    * @param size for tracking size
+    */
     @Override
     public int size() {//function to get size
         return size;
     }
+    /*
+     * @check_index() checks if index is valid
+     * @param index the index we are checking
+     * @throw throws error if index is out of bounds
+     */
     @Override
     public void check_index(int index){
         if(index > size || index < 0){
             throw new IndexOutOfBoundsException();
         }
     }
+    /*
+     * @contains() to check if element is in LinkedArray
+     * @param o the element we are checking
+     * @returns true if element is in LinkedArray, otherwise returns false
+     */
     @Override
     public boolean contains(Object o) {
         Node <E> buff = head;
@@ -34,46 +48,48 @@ public class MyLinkedList<E> implements MyList<E>{
         }
         return false;
     }
-
+    /*
+     * @add() to add element to the end of LinkedArray
+     * @param item the element we are adding
+     */
     @Override
     public void add(E item) {
         Node <E> newNode = new Node<E>(item);
-        if(tail == null){
-            head = newNode;
+        if(tail == null){ //checking if list is empty
+            head = newNode; //value for first elem
         }
         else {
-            tail.next = newNode;
-            newNode.prev = tail;
+            tail.next = newNode; //if list is not empty adding new vale to the end of list
+            newNode.prev = tail;//changing connected value for new elem
         }
-        tail = newNode;
-        size++;
+        tail = newNode;//changing last value
+        size++;//size is changed, so we increase the number to track size
     }
-
+    /*
+     * @add() to add element to the end of LinkedArray
+     * @param item the element we are adding
+     * @param index to insert new element AFTER this index
+     */
     @Override
     public void add(E item, int index) {
-        check_index(index);
+        check_index(index);//checking if index is valid
         Node <E> buff = head;
-        while(index != 0){
-            buff = buff.next;
+        while(index != 0){//loop to find our element
+            buff = buff.next;//next elem
             index--;
         }
-        Node <E> newNode = new Node<E>(item);
-        newNode.prev = buff;
-        newNode.next = buff.next;
-        if(buff.next != null){
-            buff.next.prev = newNode;
+        Node <E> newNode = new Node<E>(item); //creating new connection
+        newNode.prev = buff;//connecting new element to previous
+        newNode.next = buff.next;//connecting new element to next
+        if(buff.next != null){//checking if we are on the last element already
+            buff.next.prev = newNode;//changing connection between nexts' previous element
         }
         else{
-            tail = newNode;
+            tail = newNode;//changing last elem
         }
         buff.next = newNode;
-        size++;
+        size++;//insreasing size
     }
-
-//    @Override
-//    public boolean remove(Object item) {
-//        return false;
-//    }
 
 
     @Override
