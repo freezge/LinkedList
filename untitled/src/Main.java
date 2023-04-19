c2ce9fc372780a6f36118cbd1018bd39cf8606f7
freezge <freezge@gmail.com>
2023-04-19 21:06:38 +0600

    MyArrList sort

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,13 +3,14 @@ public class Main {
         MyArrayList a = new MyArrayList();
         a.add(1);
         a.add(2);
+        a.add(99);
         a.add(3);
         a.add(4);
         a.add(5);
         a.add(6);
         a.add(7);
-        a.remove("7");
-        a.remove(5);
+        a.add(0);
+        a.sort();
         for(int i = 0; i < a.size(); i++){
             System.out.println(a.get(i));
         }
--- untitled/src/MyArrayList.java
+++ untitled/src/MyArrayList.java
@@ -123,6 +123,16 @@ public class MyArrayList implements MyList{
 
     @Override
     public void sort() {
-
+        int buff[] = new int[arr.length];
+        int k = 0;
+        for(int i = 0; i < size - 1; i++){
+            for(int j = 0; j < size - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
     }
 }
